94b13004b23992521240b8b82a10dd068b8f1002
AJ-Tan <aldomin22@gmail.com>
2025-11-23 23:04:02 +0800

    Finished the ui for the right side of the page.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('ei');
+const nodePassword = document.getElementById('password');
+const nodeConfirmPassword = document.getElementById('confirm-password');
+
+const validatePassword = () => {
+  const password = nodePassword.value;
+  const confirmPassword = nodeConfirmPassword.value;
+  if(confirmPassword.length > 0 && confirmPassword !== password) {
+    nodeConfirmPassword.setCustomValidity('Password does not match');
+    nodeConfirmPassword.nextElementSibling.style.display = 'block';
+  } else {
+    nodeConfirmPassword.setCustomValidity('');
+    nodeConfirmPassword.nextElementSibling.style.display = 'none';
+  }
+}
+
+nodePassword.addEventListener('input', validatePassword);
+nodeConfirmPassword.addEventListener('focusout', validatePassword);
+
+// Button Create
+const nodeBtnCreate = document.getElementById('btn-create');
+
+nodeBtnCreate.addEventListener('submit', e => {
+  e.preventDefault();
+})
